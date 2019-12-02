84b9494bb7bf770406cf7721a4b84459cb0b0846
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-12-02 15:18:54 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -95,8 +95,8 @@ async def cancel_message_f(client, message):
 
 async def exec_message_f(client, message):
     if await AdminCheck(client, message.chat.id, message.from_user.id):
-        DELAY_BETWEEN_EDITS = 3
-        PROCESS_RUN_TIME = 500
+        DELAY_BETWEEN_EDITS = 0.3
+        PROCESS_RUN_TIME = 100
         cmd = message.text.split(" ", maxsplit=1)[1]
 
         reply_to_id = message.message_id
