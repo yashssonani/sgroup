22884a6eeaa995e5704f9d849d40e0c1f43baa0d
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-12-02 12:45:58 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,27 @@
+TG_BOT_TOKEN = 913300625:AAFiJusmlDuBLEw5mK1yixT8DVBQhdsufnY
+    # The Telegram API things
+    APP_ID = 1101600
+    API_HASH = f2fab3311f3e9853f6efa7ba52947080
+    # Get these values from my.telegram.org
+    # to store the channel ID who are authorized to use the bot
+    AUTH_CHANNEL = -1001404187323
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 1572864000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = 128
+    # default thumbnail to be used in the videos
+    
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    #
+    ARIA_TWO_STARTED_PORT = 6800
+    EDIT_SLEEP_TIME_OUT = 1
+    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = 600
+    MAX_TG_SPLIT_FILE_SIZE = 1072864000
+    
